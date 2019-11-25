c52314755658692a0e15b63d795ede5f196a8877
Christopher Brown <pyrrhicpachyderm@gmail.com>
2019-11-25 15:29:07 +1300

    Remove debug output from the website.

--- website/character-sheets/online/js/toggle-experience.js
+++ website/character-sheets/online/js/toggle-experience.js
@@ -1,9 +1,7 @@
 function toggleExperience(checkboxElement){
 	if(checkboxElement.checked) {
 		$(".experience").show();
-		console.log("show");
 	} else {
 		$(".experience").hide();
-		console.log("hide");
 	}
 }
