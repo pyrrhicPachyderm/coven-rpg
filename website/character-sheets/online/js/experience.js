4537e5b80f7736af17b018f4af047ca6e28806a0
Christopher Brown <pyrrhicpachyderm@gmail.com>
2019-11-25 16:38:24 +1300

    Fill the XP tracking bar on the online character sheet.

--- website/character-sheets/online/js/experience.js
+++ website/character-sheets/online/js/experience.js
@@ -1,9 +1,30 @@
 let experience = {
+	totalExperience:100,
+	
 	toggle:function(checkboxElement) {
 		if(checkboxElement.checked) {
 			$(".experience").show();
+			this.updatePage();
 		} else {
 			$(".experience").hide();
 		}
-	}
+	},
+	
+	getTotal:function() {
+		return this.totalExperience;
+	},
+	
+	getRemaining:function() {
+		return this.getTotal();
+	},
+	
+	updatePage:function() {
+		document.getElementById('experience-total').value = this.getTotal();
+		document.getElementById('experience-remaining').value = this.getRemaining();
+	},
+	
+	add:function(amount) {
+		this.totalExperience += amount;
+		this.updatePage();
+	},
 }
