pyrrhicPachyderm/coven-rpg

commit 115017b03d2d7cae6ebeed44a1e5322cf2fb0406

Break the online character sheet's Model down into nested Models.
This thoroughly breaks loading, as the nested models are
overwritten with new, basic objects, instead of Backbone objects.

website/character-sheets/online/js/character-model.js
@@ -6,20 +6,9 @@ var CharacterModel = Backbone.Model.extend({
 			playerName: "",
 			isTrackingExperience: true,
 			totalExperience: 100,
-			attributes: Array(8).fill(0),
+			stats: new StatsModel(),
 		}
 	},
-	
-	attributeIDs: [
-		"might",
-		"grace",
-		"ken",
-		"wit",
-		"will",
-		"heed",
-		"charm",
-		"presence",
-	],
 	
 	initialize: function() {
 		this.remainingExperience = this.calculateRemainingExperience();
@@ -28,12 +17,6 @@ var CharacterModel = Backbone.Model.extend({
 	reset: function() {
 		this.set(this.defaults());
 		this.initialize();
-	},
-	
-	setArrayElement: function(array, index, value) {
-		this.get(array)[index] = value;
-		this.trigger("change");
-		this.trigger("change:" + array);
 	},
 	
 	calculateRemainingExperience: function() {
@@ -57,13 +40,12 @@ var CharacterModel = Backbone.Model.extend({
 		this.set(JSON.parse(str));
 	},
 	
-	getAttribute: function(attributeID) {
-		let index = this.attributeIDs.indexOf(attributeID);
-		return this.get("attributes")[index];
+	getAttribute: function(attribute) {
+		return this.get("stats").getAttribute(attribute);
 	},
 	
-	setAttribute: function(attributeID, value) {
-		let index = this.attributeIDs.indexOf(attributeID);
-		this.setArrayElement("attributes", index, value);
+	setAttribute: function(attribute, value) {
+		this.get("stats").setAttribute(attribute, value);
+		triggerChange(this, "stats");
 	},
 });

website/character-sheets/online/js/utilities.js
@@ -1,0 +1,4 @@
+var triggerChange = function(object, attribute) {
+	object.trigger("change");
+	object.trigger("change:" + attribute);
+}

website/character-sheets/online/js/stats-model.js
@@ -1,0 +1,42 @@
+var Stats = {
+	attributeIDs: [
+		"might",
+		"grace",
+		"ken",
+		"wit",
+		"will",
+		"heed",
+		"charm",
+		"presence",
+	],
+	attributeMax: 5,
+}
+Object.freeze(Stats.attributeIDs);
+Object.freeze(Stats);
+
+var AttributesModel = Backbone.Model.extend({
+	defaults: function () {
+		let obj = {};
+		for(attributeID of Stats.attributeIDs) {
+			obj[attributeID] = 0;
+		}
+		return obj;
+	},
+});
+
+var StatsModel = Backbone.Model.extend({
+	defaults: function () {
+		return {
+			attributes: new AttributesModel(),
+		}
+	},
+	
+	getAttribute: function(attribute) {
+		return this.get("attributes").get(attribute);
+	},
+	
+	setAttribute: function(attribute, value) {
+		this.get("attributes").set(attribute, value);
+		triggerChange(this, "attributes");
+	},
+});

website/character-sheets/online/js/views.js
@@ -22,10 +22,9 @@ $(document).ready(function() {
 				$(".experience").hide();
 			}
 			
-			let attributes = this.model.get("attributes");
-			for(let i = 0; i < attributes.length; i++) {
-				let attributeID = this.model.attributeIDs[i];
-				this.createDots($("#attribute-" + attributeID), 5, attributes[i]);
+			for(attributeID of Stats.attributeIDs) {
+				let attributeScore = this.model.getAttribute(attributeID);
+				this.createDots($("#attribute-" + attributeID), Stats.attributeMax, attributeScore);
 			}
 		},
 		

website/character-sheets/online/sheetapp.html
@@ -20,6 +20,8 @@
 		
 		<link rel="stylesheet" href="css/styles.css">
 		
+		<script src="js/utilities.js"></script>
+		<script src="js/stats-model.js"></script>
 		<script src="js/character-model.js"></script>
 		<script src="js/views.js"></script>
 	</head>

commit 8288253e502ae48c0c4053a63260ac6bf7e3f66a

Rewrite the online sheet's loading function to acount for nested Models.
Fix the loading bug introduced by 115017b03d2d7cae6ebeed44a1e5322cf2fb0406.

website/character-sheets/online/js/character-model.js
@@ -32,7 +32,7 @@ var CharacterModel = Backbone.Model.extend({
 	
 	readJSON: function(str) {
 		resetModel(this);
-		this.set(JSON.parse(str));
+		loadModelFromJSON(this, JSON.parse(str));
 	},
 	
 	getAttribute: function(attribute) {

website/character-sheets/online/js/utilities.js
@@ -23,3 +23,22 @@ var resetModel = function(model) {
 	model.set(model.defaults());
 	model.initialize();
 }
+
+//Note: the below function assumes that any nesting of Backbone Models is direct.
+//i.e. Any attributes that contain Backbone Models are those Backbone Models themselves.
+//We don't have attributes that are arrays of Backbone Models, or plain JS objects containing
+//Backbone Models.
+var loadModelFromJSON = function(model, obj) {
+	resetModel(model);
+	
+	for(property in obj) {
+		if(Object.prototype.hasOwnProperty.call(obj, property)) {
+			if(model.get(property) instanceof Backbone.Model) {
+				loadModelFromJSON(model.get(property), obj[property]);
+				triggerChange(model, property);
+			} else {
+				model.set(property, obj[property]);
+			}
+		}
+	}
+}